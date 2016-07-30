582b82a6f99cd73cce6cd32ef229ceec58dc9435
ZhangChuann <zhangchuan402@gmail.com>
2016-07-30 17:34:58 +0800

    change 7.30

--- www/test_db.py
+++ www/test_db.py
@@ -16,7 +16,10 @@ print 'new user id:', u.id
 u1 = User.find_first('where email=?', 'test@example.com')
 print 'find user\'s name:', u1.name
 
-u1.delete()
+#u1.delete()
 
 u2 = User.find_first('where email=?', 'test@example.com')
 print 'find user:', u2
+
+
+u = Blog()
